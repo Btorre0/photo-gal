61e88c2302dd3bc23f480b42effb9b8f43dc5c3c
Beatriz Torres <btorre0@csu.fullerton.edu>
2025-03-08 21:09:12 -0800

    added function and base of the website. Need to fix when openning the site (lightbox opens right off the bat without any image)

---
+++ script.js
@@ -0,0 +1,53 @@
+const images = [
+    "gallery/IMG_0010.png",
+    "gallery/IMG_0015.png",
+    "gallery/IMG_0039.png",
+    "gallery/IMG_0131.png",
+    "gallery/IMG_0132.png",
+    "gallery/IMG_0133.png",
+    "gallery/IMG_0140.png",
+    "gallery/IMG_0141.png",
+    "gallery/IMG_0310.png",
+    "gallery/IMG_0313.png",
+    "gallery/IMG_0440.png",
+    "gallery/IMG_0445.png"
+];
+
+let currentIndex = 0;
+
+function openLightbox(index, event) {
+    event.preventDefault();
+    currentIndex = index;
+    document.getElementById("lightbox-img").src = images[currentIndex];
+    document.getElementById("lightbox").style.display = "flex";
+}
+
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
+// Change image in lightbox
+function changeImage(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    } else if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    document.getElementById("lightbox-img").src = images[currentIndex];
+}
+
+// Keyboard navigation support
+document.addEventListener("keydown", function(event) {
+    document.getElementById("lightbox").style.display = "none";
+    if (document.getElementById("lightbox").style.display === "block") {
+        if (event.key === "ArrowRight") {
+            changeImage(1);
+        } else if (event.key === "ArrowLeft") {
+            changeImage(-1);
+        } else if (event.key === "Escape") {
+            closeLightbox();
+        }
+    }
+});
